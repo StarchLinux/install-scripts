be6b7ce33cf106ed409108a44d25e636263191a4
Strake <strake888@gmail.com>
2013-02-24 08:54:28 -0500

    note mounts, derp

--- common.sh
+++ common.sh
@@ -4,13 +4,13 @@ function mount_noted {
 
 function api_fs_mount {
 	NOTED_MOUNTS=()
-	mount -t proc		_ "$1/proc"	-o nosuid,noexec,nodev		&&
-	mount -t sysfs		_ "$1/sys"	-o nosuid,noexec,nodev		&&
-	mount -t devtmpfs	_ "$1/dev"	-o mode=0755,nosuid		&&
-	mount -t devpts		_ "$1/dev/pts"	-o mode=0620,nosuid,noexec	&&
-	mount -t tmpfs		_ "$1/dev/shm"	-o mode=1777,nosuid,nodev	&&
-	mount -t tmpfs		_ "$1/run"	-o mode=0755,nosuid,nodev	&&
-	mount -t tmpfs		_ "$1/tmp"	-o mode=1777,nosuid,nodev
+	mount_noted -t proc	_ "$1/proc"	-o nosuid,noexec,nodev		&&
+	mount_noted -t sysfs	_ "$1/sys"	-o nosuid,noexec,nodev		&&
+	mount_noted -t devtmpfs	_ "$1/dev"	-o mode=0755,nosuid		&&
+	mount_noted -t devpts	_ "$1/dev/pts"	-o mode=0620,nosuid,noexec	&&
+	mount_noted -t tmpfs	_ "$1/dev/shm"	-o mode=1777,nosuid,nodev	&&
+	mount_noted -t tmpfs	_ "$1/run"	-o mode=0755,nosuid,nodev	&&
+	mount_noted -t tmpfs	_ "$1/tmp"	-o mode=1777,nosuid,nodev
 }
 
 function api_fs_umount {
